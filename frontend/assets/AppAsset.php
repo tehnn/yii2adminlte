9c53214367492e96b819e77ab9e8110a764990c5
UTEHN <UTEHN@UTEHN-NB>
2015-01-01 18:55:38 +0700

    2015-01-01

---
+++ frontend/assets/AppAsset.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace frontend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+/*
+  class AppAsset extends AssetBundle
+  {
+  public $basePath = '@webroot';
+  public $baseUrl = '@web';
+  public $css = [
+  'css/site.css',
+  ];
+  public $js = [
+  ];
+  public $depends = [
+  'yii\web\YiiAsset',
+  'yii\bootstrap\BootstrapAsset',
+  ];
+  } */
+
+class AppAsset extends AssetBundle {
+
+    public $sourcePath = '@theme/';
+    public $css = array(
+        //'admin-lte/css/bootstrap.min.css',
+        'admin-lte/css/AdminLTE.css',
+        'admin-lte/css/font-awesome.min.css',
+        'admin-lte/css/ionicons.min.css'
+    );
+    public $js = array(
+        'admin-lte/js/AdminLTE/app.js',
+    );
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
+    ];
+
+}
