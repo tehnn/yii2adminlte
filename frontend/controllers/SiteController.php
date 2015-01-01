251d7bab504c73754150635e1b738eb84dbdd9a7
UTEHN <UTEHN@UTEHN-NB>
2015-01-01 19:03:39 +0700

    2015-01-01

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -90,7 +90,7 @@ class SiteController extends Controller
     {
         Yii::$app->user->logout();
 
-        return $this->goHome();
+        return $this->redirect(array('login'));
     }
 
     public function actionContact()
